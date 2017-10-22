100103c7ccd37ab8c438824dc177b8cfd8ed0ed9
sovofeel <sovofeel@gmail.com>
2017-10-22 15:46:13 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+
+
+const App = () => (
+  <div className="ui container">
+    <Route path="/" exact  component={HomePage}/>
+    <Route path="/login"  component={LoginPage}/>
+  </div>
+)
+
+export default App;
